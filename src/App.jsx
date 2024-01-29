8e13223b13f86862a3145a45eb8882df1a34eaa3
Siv Earley <searley96@gmail.com>
2024-01-29 10:52:03 -0600

    initialized project with react and vite, assed search input to home page

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useRef } from 'react';
+import { Form } from 'react-bootstrap';
+import './index.css';
+
+const App = () => {
+  const searchInput = useRef(null);
+  return (
+    <div className='container'>
+      <h1 className='title'>Image Search</h1>
+      <div className='search-section'>
+        <Form>
+          <Form.Control
+            type='search'
+            placeholder='Type something to search...'
+            className='search-input'
+            ref={searchInput}
+          />
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default App;
