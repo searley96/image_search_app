b99fb6be0f0dc53c7cf44aa206b26bd33229a385
Siv Earley <searley96@gmail.com>
2024-01-31 10:57:44 -0600

    created display buttons that can be clicked to search images.

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,14 @@ const App = () => {
 
 const handleSearch = (event) => {
   event.preventDefault();
-  console.log("submitted");
+  console.log(searchInput.current.value);
 };
 
+const handleSelection = (selection) => {
+  searchInput.current.value = selection;
+}
+//Once the form is submitted by pressing the enter key in the search box, the page will not refresh and a submitted text will be displayed in the console
+//enter any search term in the search box and press the enter key
   return (
     <div className="container">
       <h1 className="title">Image Search</h1>
@@ -22,6 +27,12 @@ const handleSearch = (event) => {
             ref={searchInput}
           />
         </Form>
+      </div>
+      <div className="filters">
+        <div onClick={() => handleSelection('nature')}>Nature</div>
+        <div onClick={() => handleSelection('birds')}>Birds</div>
+        <div onClick={() => handleSelection('cats')}>Cats</div>
+        <div onClick={() => handleSelection('shoes')}>Shoes</div>
       </div>
     </div>
   );
