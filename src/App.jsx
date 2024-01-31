cbf4c84181492a0951b782cbbe6232ae5197ba67
Siv Earley <searley96@gmail.com>
2024-01-31 13:51:49 -0600

    added pagination, created buttons, new images load for new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-import React, { useRef, useState } from "react";
-import { Form } from "react-bootstrap";
+import React, { useRef, useState, useEffect } from "react";
+import { Form, Button } from "react-bootstrap";
 import "./index.css";
 
 const API_URL = "https://api.unsplash.com/search/photos";
@@ -9,8 +9,17 @@ const IMAGES_PER_PAGE = 20;
 
 const App = () => {
   const searchInput = useRef(null);
+
   const [images, setImages] = useState([]);
   const [totalPages, setTotalPages] = useState(0);
+  const [page, setPage] = useState(1);
+
+  //when previous or next button clicked, page value changes
+  // useEffect hook executed when page value changes
+  // fetchImages() called, loads next set of images
+  useEffect(() => {
+    fetchImages();
+  }, [page]);
 
   // define fetchImages function which is declared async so we can use await inside of it
   // inside fetchImages function, make a GET API call using axios to the URL in which API_URL constant is stored
@@ -21,11 +30,11 @@ const App = () => {
       const { data } = await axios.get(
         `${API_URL}?query=${
           searchInput.current.value
-        }&page=1&per_page=${IMAGES_PER_PAGE}&client_id=${
+        }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
           import.meta.env.VITE_API_KEY
         }`
       );
-      console.log('data', data);
+      console.log("data", data);
       setImages(data.results);
       setTotalPages(data.total_pages);
     } catch (error) {
@@ -33,16 +42,27 @@ const App = () => {
     }
   };
 
-  const handleSearch = (event) => {
-    event.preventDefault();
-    console.log(searchInput.current.value);
+  const resetSearch = (event) => {
+    setPage(1);
     fetchImages();
   };
 
+  // when keyword is searched, laod images for that keyword
+  // reset to page one so only 'next' button visable
+  const handleSearch = (event) => {
+    event.preventDefault();
+    console.log(searchInput.current.value);
+    resetSearch();
+  };
+
+  // when filter is selected, laod images for that keyword
+  // reset to page one so only 'next' button visable
   const handleSelection = (selection) => {
     searchInput.current.value = selection;
-    fetchImages();
+    resetSearch();
   };
+
+  console.log("page", page);
 
   // once the form is submitted by pressing the enter key in the search box, the page will not refresh and a submitted text will be displayed in the console
   // enter any search term in the search box and press the enter key
@@ -75,6 +95,14 @@ const App = () => {
             className="image"
           />
         ))}
+      </div>
+      <div className="buttons">
+        {page > 1 && (
+          <Button onClick={() => setPage(page - 1)}>Previous</Button>
+        )}
+        {page < totalPages && (
+          <Button onClick={() => setPage(page + 1)}>Next</Button>
+        )}
       </div>
     </div>
   );
